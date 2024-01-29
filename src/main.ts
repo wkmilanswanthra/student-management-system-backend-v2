f12743438b4e6d50b647c5cc974b792d1591a734
Milan Swanthra <91304603+wkmilanswanthra@users.noreply.github.com>
2024-01-29 19:34:13 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api/v2');
-  await app.listen(3000);
+  await app.listen(process.env.RDS_USERNAME);
 }
 bootstrap();
